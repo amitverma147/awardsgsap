ab6ec99535dcfe8cd7c2c15db2c38eecab6fe3c5
Amit Verma <vamit20000@gmail.com>
2025-06-30 17:41:56 +0530

    smooth scrolling

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -2,24 +2,62 @@ import { useGSAP } from "@gsap/react";
 import { flavorlists } from "../constants";
 import gsap from "gsap";
 import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const FlavorSlider = () => {
   const sliderRef = useRef();
+  const isTablet = useMediaQuery({
+    query: "(max-width:1024px)",
+  });
   useGSAP(() => {
-    const scrollAmout = sliderRef.current.scrollWidth - window.innerWidth;;
-    const tl = gsap.timeline({
+    const scrollAmout = sliderRef.current.scrollWidth - window.innerWidth;
+
+    if (!isTablet) {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".flavor-section",
+          start: "2% top",
+          end: `+=${scrollAmout + 1500}px`,
+          scrub: true,
+          pin: true,
+        },
+      });
+      tl.to(".flavor-section", {
+        x: `-${scrollAmout + 1500}px`,
+        ease: "power1.inOut",
+      });
+    }
+
+    const titleTl = gsap.timeline({
       scrollTrigger: {
         trigger: ".flavor-section",
-        start: "2% top",
-        end: `+=${scrollAmout+1500}px`,
+        start: "top top",
+        end: "bottom 80%",
         scrub: true,
-        pin:true,
       },
     });
-    tl.to(".flavor-section", {
-      x: `-${scrollAmout+1500}px`,
-      ease: "power1.inOut",
-    });
+
+    titleTl
+      .to(".first-text-split", {
+        xPercent: -30,
+        ease: "power1.inOut",
+      })
+      .to(
+        ".flavor-text-scroll",
+        {
+          xPercent: -22,
+          ease: "power1.inOut",
+        },
+        "<"
+      )
+      .to(
+        ".second-text-split",
+        {
+          xPercent: -10,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
   });
 
   return (
